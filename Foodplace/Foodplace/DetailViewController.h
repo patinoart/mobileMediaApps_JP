f0e0dad587d1638331bd16c0fd2cee0617dcfe59
Juan Patino <patinoart@gmail.com>
2013-04-02 01:36:47 -0400

    New App, Assignment 1 for MM

---
+++ Foodplace/Foodplace/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Foodplace
+//
+//  Created by Juan Patino on 4/1/13.
+//  Copyright (c) 2013 Juan Patino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
